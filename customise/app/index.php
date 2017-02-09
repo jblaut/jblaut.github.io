296c616aa3662c6f48fb8a55985c0ff0a137477f
J Blaut <jagoda.blaut@bbc.co.uk>
2017-02-09 12:32:16 +0000

    change layout

--- customise/app/index.php
+++ customise/app/index.php
@@ -1,38 +1,90 @@
-<!doctype html>
-<html lang="en-GB">
-  <head>
-    <meta charset="utf-8">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Customise</title>
+<!DOCTYPE html>
+<html lang="en-gb">
+<title>Customise - Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: 'Josefin Sans', Arial, sans-serif;}
+.myLink {display: none}
+</style>
+<body class="w3-light-grey">
 
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="stylesheet" href="styles/main.css">
-  </head>
-  <body class="index">
-    <!--[if lt IE 10]>
-      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-    <nav>
-      <ul>
-        <a href="/"><li>Home</li></a>
-        <a href="/features.html"><li>Features</li></a>
-        <a href="/user-area.html"><li>User Area</li></a>
-      </ul>
-    </nav>
-    <div class="hero-unit">
-      <h1>Customise</h1>
-      <p>Welcome to the customise service. Here you'll be able to:</p>
-      <ul>
-        <li>View most popular movies based on genres you like</li>
-        <li>See movie history of your favourite actors</li>
-        <li>Create your own To Watch list</li>
-      </ul>
+<!-- Navigation Bar -->
+<ul class="w3-navbar w3-white w3-border-bottom w3-xlarge">
+  <li><a class="w3-text-white w3-hover-red"><b><i class="fa fa-map-marker w3-margin-right"></i>Customise</b></a></li>
+  <li><a href="/" class="w3-text-white w3-hover-blue-grey">Home</a></li>
+  <li><a href="features.html" class="w3-text-white w3-hover-blue-grey">Features</a></li>
+  <li><a href="user-area.html" class="w3-text-white w3-hover-blue-grey">User Area</a></li>
+</ul>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-hide-small" style="max-width:1500px">
+  <img class="w3-image" src="images/placeholder_main.jpg" alt="London" width="1500" height="700">
+  <div class="w3-display-middle" style="width:65%">
+
+    <!-- Tabs -->
+    <div id="Flight" class="w3-container w3-white w3-padding-16">
+      <h3>Search by Actor or Movie Title</h3>
+      <div class="w3-row-padding" style="margin:0 -16px;">
+        <div class="w3-threequarter">
+          <input class="w3-input w3-border" type="text" placeholder="Departing from">
+        </div>
+        <div class="w3-quarter">
+          <button class="w3-btn w3-dark-grey">Search</button>
+        </div>
+      </div>
     </div>
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-    <script src="scripts/main.js"></script>
-  </body>
-  <footer>
-    <p>© J Blaut 2017</p>
-  </footer>
+  </div>
+</header>
+
+  <div class="w3-content w3-container w3-padding-64" id="about">
+    <h3 class="w3-center">ABOUT THE WEBSITE</h3>
+    <p>
+      We have created a fictional "personal" website/blog, and our fictional character is a hobby photographer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+      qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+
+  <div class="w3-row w3-center w3-dark-grey w3-padding-16">
+    <div class="w3-quarter w3-section">
+      <span class="w3-xlarge">140,000+</span><br>
+      Actors
+    </div>
+    <div class="w3-quarter w3-section">
+      <span class="w3-xlarge">20,000+</span><br>
+      Movies
+    </div>
+    <div class="w3-quarter w3-section">
+      <span class="w3-xlarge">10,000+</span><br>
+      Users
+    </div>
+    <div class="w3-quarter w3-section">
+      <span class="w3-xlarge">1</span><br>
+      Website
+    </div>
+  </div>
+
+<!-- Footer -->
+<footer class="w3-container w3-center w3-opacity w3-margin-bottom">
+  <h5>Find Us On</h5>
+  <div class="w3-xlarge w3-padding-16">
+    <i class="fa fa-facebook-official w3-hover-text-indigo"></i>
+    <i class="fa fa-instagram w3-hover-text-purple"></i>
+    <i class="fa fa-snapchat w3-hover-text-yellow"></i>
+    <i class="fa fa-pinterest-p w3-hover-text-red"></i>
+    <i class="fa fa-twitter w3-hover-text-light-blue"></i>
+    <i class="fa fa-linkedin w3-hover-text-indigo"></i>
+  </div>
+  <!-- <p>Powered by <a href="http://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p> -->
+</footer>
+
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="scripts/main.js"></script>
+
+</body>
 </html>
