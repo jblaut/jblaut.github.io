1c37171d7ca50673a70b162714ebea8d1b8bdb35
J Blaut <jagoda.blaut@bbc.co.uk>
2017-03-27 15:02:20 +0100

    protect against SQL injections, create temp files with recent data

--- personalise/serverwithflask.py
+++ personalise/serverwithflask.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask
 from flask import request
 from flask import Response
@@ -32,39 +34,42 @@ except:
 separator = '::'
 app = Flask(__name__)
 
+with open('ratings.dat', 'r') as content_file:
+    content = content_file.read()
+
 @app.route('/data')
 def query():
     user = request.args.get('user_id')
     processedRow = ''
     hash_ids = []
     item_count = 0
-    
+
     with conn.cursor() as cur:
-        query = "SELECT movie_id, rating FROM recommendationDB.user_ratings WHERE user_id=" + user
+        query = "SELECT movie_id, rating FROM recommendationDB.user_ratings WHERE user_id=%s"
         print query
-        cur.execute(query)
+    	cur.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
+        cur.execute(query, (user))
         for row in cur:
             item_count += 1
             processedRow = processedRow + str(user) + separator + str(row[0]) + separator + str(row[1]) + separator + str(int(time.time())) + "\r\n"
             hash_ids.append(str(row[0]))
         print processedRow
-        text_file = open("ratings.dat", "a")
-        text_file.write(processedRow)
+        hash = hashlib.sha1('.'.join(hash_ids)).hexdigest()
+        text_file = open("test/" + hash + ".dat", "w+")
+        text_file.write(content + processedRow)
         text_file.close()
-    
-    print hash_ids
-    hash = hashlib.sha1('.'.join(hash_ids)).hexdigest()
+
     print(hash)
 
     cache = memcache_client.get(hash)
     print cache
-    if cache != None:    
+    if cache != None:
         return Response(response=json.dumps({"results":json.loads(cache)}),
                         status=200,
                         mimetype="application/json")
     else:
         svd = SVD()
-        svd.load_data(filename='./ratings.dat',
+        svd.load_data(filename='./test/' + hash + '.dat',
         sep=separator,
         format={'col':0, 'row':1, 'value':2, 'ids': int})
 
@@ -77,13 +82,12 @@ def query():
 
         film_titles = []
         with conn.cursor() as cur:
-            query = "SELECT movie_id, movie_title, movie_genre FROM recommendationDB.movies WHERE " + films 
+            query = "SELECT movie_id, movie_title, movie_genre FROM recommendationDB.movies WHERE " + films
             print query
             cur.execute(query)
             for row in cur:
                 item_count += 1
                 film_titles.append({"title": row[1],"genre": row[2], "relevance": recommendations_dict[row[0]], "movie_id": row[0]})
-
         memcache_client.set(hash, json.dumps(film_titles))
 
         return Response(response=json.dumps({"results":film_titles}),
