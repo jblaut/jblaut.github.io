f507def0973939f5813192a44e7c8c68e01aba01
J Blaut <jagoda.blaut@bbc.co.uk>
2017-03-30 20:21:20 +0100

    save response to file to limit the number of requests + cron job

---
+++ customise/app/includes/savefile.php
@@ -0,0 +1,17 @@
+<?php
+$service_url = 'http://www.myapifilms.com/imdb/top?start=1&end=250&token=72dff507-0f89-4d9a-b53b-6f83e8d7e6ac&format=json&data=1';
+$curl = curl_init($service_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($curl);
+$results = json_decode($curl_response);
+
+$fp = fopen('response.json', 'w+');
+fwrite($fp, json_encode($results));
+fclose($fp);
+?>
