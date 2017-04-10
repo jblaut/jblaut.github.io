b706568558b52fad387743c0841115de79f9ff18
J Blaut <jagoda.blaut@bbc.co.uk>
2017-04-10 01:29:44 +0100

    remove log out button hovering

--- customise/app/scripts/loggedIn.js
+++ customise/app/scripts/loggedIn.js
@@ -1,11 +1,3 @@
 if (loggedIn == 'true' && username != 'false') {
 	$('#loggedIn').show();
-	$('#loggedIn').append(" " + username.toUpperCase());
-	
-	$("#loggedIn").mouseenter(function(){
-		$('#loggedIn').html("Log Out");
-	});
-	$("#loggedIn").mouseleave(function(){
-			$('#loggedIn').html('<i class="fa fa-user"></i> ' + username.toUpperCase());
-	});
 }
