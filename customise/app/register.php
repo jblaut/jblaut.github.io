3da8a26a6c0276ad26251fc5cd4a57504db5c1c2
J Blaut <jagoda.blaut@bbc.co.uk>
2017-04-10 01:29:13 +0100

    add date to created account

--- customise/app/register.php
+++ customise/app/register.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 	session_start();
 
 	include 'includes/dbConnection.php';
-	
+
 	$username = $_REQUEST['username'];
 	$email = $_REQUEST['email'];
 	$password = md5($_REQUEST['password']);
-	
+
 	$currentUsers = "SELECT * FROM users;";
 	$result = mysqli_query($con, $currentUsers);
-	
+
 	while($row = mysqli_fetch_array($result)) {
 		if ($username == $row['userName']) {
 			$_SESSION['registerError'] = 'Username already exists.';
@@ -21,10 +21,10 @@
 			die();
 		}
 	}
-	
-	$newUserQuery = "INSERT INTO users (userName, userEmail, userPassword) VALUES ('" . $username . "', '" . $email . "', '" . $password . "');";
+
+	$newUserQuery = "INSERT INTO users (userName, userEmail, userPassword, dateCreated) VALUES ('" . $username . "', '" . $email . "', '" . $password . "', '" . date('m/d/Y h:i:s a', time()) ."');";
 	mysqli_query($con, $newUserQuery);
-	
+
 	$select = "SELECT * FROM users WHERE userName = '$username' AND userEmail = '$email';";
 	$selectRunQuery = mysqli_query($con, $select);
 	$row = mysqli_fetch_array($selectRunQuery, MYSQLI_ASSOC);
@@ -33,6 +33,6 @@
 	$_SESSION['registerSuccess'] = "You have successfully created your account. You can log in now!";
 
 	mysqli_close($con);
-	
+
 	header('Location: user-area.php');
 ?>
