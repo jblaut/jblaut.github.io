56a31011d38da0651354ff4c900266b7284275a7
Jamie Blaut <jagoda.blaut@bbc.co.uk>
2017-04-21 16:17:33 +0100

    Update savefile.php

--- customise/app/includes/savefile.php
+++ customise/app/includes/savefile.php
@@ -1,4 +1,5 @@
 <?php
+# code from https://support.ladesk.com/061754-How-to-make-REST-calls-in-PHP?r=1
 $service_url = 'http://www.myapifilms.com/imdb/top?start=1&end=250&token=72dff507-0f89-4d9a-b53b-6f83e8d7e6ac&format=json&data=1';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
